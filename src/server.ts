8d9fdc735b44f1bf5f92979103dd5c899d2b7875
Rafael Pignataro <rafael.pignataro@singu.com.br>
2022-03-10 14:41:53 -0300

    feat: Added cors to the api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'express-async-errors';
-
 import express from 'express';
+import cors from 'cors';
 
 import { errorHandler } from './middlewares/errorHandler';
 
 import { routes } from './routes';
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
