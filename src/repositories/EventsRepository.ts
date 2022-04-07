4d1f68203fcae3917b2ee38cb8a8055ccfc4c95c
Rafael Pignataro <rafael.pignataro@singu.com.br>
2022-04-07 00:41:06 -0300

    feat: Added summary function to eventsRepository

--- src/repositories/EventsRepository.ts
+++ src/repositories/EventsRepository.ts
@@ -16,10 +16,43 @@ export type UpdateEventDTO = {
   eventCategoryId: number;
 }
 
+export type EventSummaryDTO = {
+  id: number;
+  version: number;
+  brands: {
+    id: number;
+    user: {
+      id: number;
+      name: string;
+      email: string;
+    };
+  }[];
+  locations: {
+    id: number;
+    name: string;
+    description: string;
+    latitude: number;
+    longitude: number;
+    locationCategory: {
+      code: string;
+      name: string;
+    };
+    activations: {
+      id: number;
+      name: string;
+      description: string;
+      startDate: Date;
+      endDate: Date;
+    }[];
+  }[];
+}
+
 export interface EventsRepository {
   findById(id: number): Promise<Event>
 
   findAll(): Promise<Event[]>
+
+  summary(id: number): Promise<EventSummaryDTO>
 
   create(data: CreateEventDTO): Promise<Event>
 
